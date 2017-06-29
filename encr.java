070c85a9dfffd68c53974a8c4b117c0853219f63
Naveed Jeelani Khan <naveed1313@gmail.com>
2017-06-29 19:08:25 +0530

    Update encr.java

--- encr.java
+++ encr.java
@@ -1,4 +1,5 @@
 //Code by Naveed Jeelani,Please give feedback if it was useful
+
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -8,8 +9,19 @@ class encr
     public static void main(String[] args) 
     {
     	try{
-    		
-    		RandomAccessFile in = new RandomAccessFile("test.jpg", "rw");  //input file to be encrypted 
+    				//Get Filename from USER to be Encrypted
+    		Scanner fnamescanner =new Scanner(System.in);
+    		System.out.println("Enter Name of the File to be Encrypted:");
+            String filename = fnamescanner.next();
+
+    		File chk = new File(filename);	//File Handler to check if any file of that name exists			
+	if(!chk.exists())	
+		{
+				System.out.println("The File [" +filename+"] doesnot exist.Check Again!");	
+		}
+	else
+		{
+    		RandomAccessFile in = new RandomAccessFile(filename, "rw");  //input file to be encrypted 
 	    	RandomAccessFile temp = new RandomAccessFile("tmp.txt", "rw"); //use as intermediate, to hold the file XOR-ed with key
 	    	RandomAccessFile out = new RandomAccessFile("enc.txt", "rw"); // used to hold the shuffled the XOR-ed file
 
@@ -17,26 +29,24 @@ class encr
 	    	System.out.println("No of characters in file:"+incount);
 	    	
 	    	
-	    	//USING KEY
-	    
-	    	Scanner scanner =new Scanner(System.in);
-		System.out.println("Enter Your Key to encrypt the file: ");
-		String key=scanner.nextLine();
-		
-		int len = key.length();
-		System.out.println("The Length of key is:"+len);
-		int sum= 0;
-		
-		for(int i=0; i<=len-1;i++)
+	    	//Get Desired Private Key from USER
+	    	Scanner keyscanner =new Scanner(System.in);
+			System.out.println("Enter Your Key to encrypt the file: ");
+			String key=keyscanner.nextLine();
+	
+				//Calculating Sum of the Key
+			int len = key.length();
+			System.out.println("The Length of key is:"+len);
+			int sum= 0;
+			for(int i=0; i<=len-1;i++)
 			{
-				int k=key.charAt(i);				
+				int k=key.charAt(i);
 				sum = sum + k;		//Getting sum of the key =>charAt() returns integer value
 			}
 
 			//Encrypt using key
-			int chr=0;
-			
-		for(int j=0; j<=incount-1;j++)
+			int chr=0;	
+			for(int j=0; j<=incount-1;j++)
 			{
 				chr=in.read();				//read from input file
 				temp.write(chr^sum);		//XOR the char and write to TEMP file
@@ -44,23 +54,23 @@ class encr
 			
 			long count=temp.length();		
 
-			//APLLY SHUFFLING
+			//APPLLY SHUFFLING
 	    
 		   int p=0;
 	    	for(long i=count-1;i>=0;i--)	
 			{
-					//PERCENTAGE LOGIC >>
-				long m=count-1;
+	    			//PERCENTAGE LOGIC >>
+	    		long m=count-1;
 				double per = (p*100.0)/m; 
-				per = per * 100;	//get 2 places from Right side of decimal to Left side
-				per = Math.round(per);			//Drop the Decimal part
-				per = per/100;					//get 2 places back to right side
-				System.out.println("Encrypting characters to File:"+per + "%");
+		        per = per * 100;				//get 2 places from Right side of decimal to Left side
+		        per = Math.round(per);			//Drop the Decimal part
+		        per = per/100;					//get 2 places back to right side
+		        System.out.println("Encrypting characters to File:"+per + "%");
 	    		
 	    		
 	    		    //Writing on file>>
-	    		 	temp.seek(i);		//set cursor of TEMP file at last >> i=count-1
-				out.seek(p);		//set cursor of output file to start >> p=0
+	    		 temp.seek(i);		//set cursor of TEMP file at last >> i=count-1
+				 out.seek(p);		//set cursor of output file to start >> p=0
 				int ch =temp.read(); //read() from TEMP
 				out.write(ch);		//write it at start of output file
 				
@@ -68,6 +78,7 @@ class encr
 			}
 	    	
 	    	 System.out.println("\nFile ENCRYPTED Successfully!");
+	    	 System.out.println("Encrypted File name is 'enc.txt'");
 
 	    	 
 	    	//delete the TEMP file
@@ -78,13 +89,16 @@ class encr
 	    	 in.close();
 	    	 out.close();
 	    	 temp.close();
-	    	 scanner.close();
+	    	 fnamescanner.close();
+	    	 keyscanner.close();
+		
 		        
-	    	} catch ( IOException e) {
+	    	}
+    	}
+	catch ( IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-
     	
     }
     
